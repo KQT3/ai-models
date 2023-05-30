d10e5d376097271e4703ee2f70d7b2ceae95523b
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-05-30 20:08:40 +0100

    commit

--- text-classification/README.md
+++ text-classification/README.md
@@ -2,11 +2,3 @@
 docker run -it --rm --gpus all --network host -v ${PWD}:/workdir -w /workdir tensorflow/tensorflow:latest-gpu bash -c "\
 pip install -r requirements.txt && bash"
 ```
-
-### Setup
-
-```
-conda create --name quickstart
-conda activate quickstart
-pip install -r requirements.txt
-```
---
+++ text-classification/main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+
+def print_hi(name):
+    print(f'Hi, {name}')
+    print(tf.__version__)
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
