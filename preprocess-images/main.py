24f1d23c9ca0e2ed5b03b9f47ebfb31214011cc8
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-06-13 18:43:36 +0100

    commit

--- preprocess-images/main.py
+++ preprocess-images/main.py
@@ -50,7 +50,126 @@ def main():
             plt.imshow(images[i].numpy().astype("uint8"))
             plt.title(class_names[labels[i]])
             plt.axis("off")
+
+    for image_batch, labels_batch in train_ds:
+        print(image_batch.shape)
+        print(labels_batch.shape)
+        break
+
+    normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+    print(np.min(first_image), np.max(first_image))
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    num_classes = 5
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Rescaling(1. / 255),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(num_classes)
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+
+    model.fit(train_ds, validation_data=val_ds, epochs=3)
+
+    list_ds = tf.data.Dataset.list_files(str(data_dir / '*/*'), shuffle=False)
+    list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+    for f in list_ds.take(5):
+        print(f.numpy())
+
+    class_names = np.array(sorted([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"]))
+    print(class_names)
+
+    val_size = int(image_count * 0.2)
+    train_ds = list_ds.skip(val_size)
+    val_ds = list_ds.take(val_size)
+
+    print(tf.data.experimental.cardinality(train_ds).numpy())
+    print(tf.data.experimental.cardinality(val_ds).numpy())
+
+    def get_label(file_path):
+        parts = tf.strings.split(file_path, os.path.sep)
+        one_hot = parts[-2] == class_names
+        return tf.argmax(one_hot)
+
+    def decode_img(img):
+        img = tf.io.decode_jpeg(img, channels=3)
+        return tf.image.resize(img, [img_height, img_width])
+
+    def process_path(file_path):
+        label = get_label(file_path)
+        img = tf.io.read_file(file_path)
+        img = decode_img(img)
+        return img, label
+
+    train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+    val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+    for image, label in train_ds.take(1):
+        print("Image shape: ", image.numpy().shape)
+        print("Label: ", label.numpy())
+
+    def configure_for_performance(ds):
+        ds = ds.cache()
+        ds = ds.shuffle(buffer_size=1000)
+        ds = ds.batch(batch_size)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
+
+    train_ds = configure_for_performance(train_ds)
+    val_ds = configure_for_performance(val_ds)
+
+    image_batch, label_batch = next(iter(train_ds))
+
+    plt.figure(figsize=(10, 10))
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(image_batch[i].numpy().astype("uint8"))
+        label = label_batch[i]
+        plt.title(class_names[label])
+        plt.axis("off")
+
+    model.fit(train_ds, validation_data=val_ds, epochs=3)
+
+    (train_ds, val_ds, test_ds), metadata = tfds.load(
+        'tf_flowers',
+        split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+        with_info=True,
+        as_supervised=True,
+    )
+
+    get_label_name = metadata.features['label'].int2str
+
+    image, label = next(iter(train_ds))
+    _ = plt.imshow(image)
+    _ = plt.title(get_label_name(label))
     plt.show()
+
+    train_ds = configure_for_performance(train_ds)
+    val_ds = configure_for_performance(val_ds)
+    test_ds = configure_for_performance(test_ds)
+
+    print(train_ds)
+    print(val_ds)
+    print(test_ds)
 
 
 if __name__ == '__main__':
